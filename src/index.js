aa44fd9ec90f6611a0aa79e250d8b30747be9033
zawho <zthooper2@gmail.com>
2023-10-24 11:09:29 -0500

    Clean up email validation funcs

--- src/index.js
+++ src/index.js
@@ -17,44 +17,38 @@ function removeFocusStyle() {
 }
 
 function validateEmailFocusOut() {
-    const emailInput = document.querySelector('.email-input');
     const emailErrorMsg = document.querySelector('#email-error-msg');
-    if (emailInput.validity.typeMismatch) {
+    if (this.validity.typeMismatch) {
         emailErrorMsg.innerText = 'please enter a valid email address.';
-        emailInput.style.border = '1px solid red';
-        emailInput.removeEventListener('focus', addValidFocusStyle);
-        emailInput.removeEventListener('focus', addNeutralFocusStyle);
-        emailInput.addEventListener('focus', addInvalidFocusStyle);
-        emailInput.addEventListener('focusout', removeFocusStyle);
-    } else if (emailInput.validity.valueMissing) {
-        emailInput.style.border = '1px solid grey';
-        emailInput.addEventListener('focus', addNeutralFocusStyle);
+        this.style.border = '1px solid red';
+        this.removeEventListener('focus', addValidFocusStyle);
+        this.removeEventListener('focus', addNeutralFocusStyle);
+        this.addEventListener('focus', addInvalidFocusStyle);
+        this.addEventListener('focusout', removeFocusStyle);
     }
 }
 
 function resetNeutralStyles() {
-    const emailInput = document.querySelector('.email-input');
     const emailErrorMsg = document.querySelector('#email-error-msg');
-    if (emailInput.validity.valueMissing) {
-        emailInput.style.border = '1px solid grey';
+    if (this.validity.valueMissing) {
+        this.style.border = '1px solid grey';
         this.style.outline = '2px solid blue';
-        emailInput.addEventListener('focus', addNeutralFocusStyle);
-        emailInput.removeEventListener('focus', addValidFocusStyle);
-        emailInput.removeEventListener('focus', addInvalidFocusStyle);
+        this.addEventListener('focus', addNeutralFocusStyle);
+        this.removeEventListener('focus', addValidFocusStyle);
+        this.removeEventListener('focus', addInvalidFocusStyle);
         emailErrorMsg.innerText = '';
     }
 }
 
 function validateEmailInput() {
-    const emailInput = document.querySelector('.email-input');
     const emailErrorMsg = document.querySelector('#email-error-msg');
-    if (emailInput.validity.valid) {
-        emailInput.style.border = '1px solid green';
-        emailInput.style.outline = '2px solid green';
-        emailInput.addEventListener('focus', addValidFocusStyle);
-        emailInput.addEventListener('focusout', removeFocusStyle);
+    if (this.validity.valid) {
+        this.style.border = '1px solid green';
+        this.style.outline = '2px solid green';
+        this.addEventListener('focus', addValidFocusStyle);
+        this.addEventListener('focusout', removeFocusStyle);
     }
-    if (emailInput.validity.valid && !(emailErrorMsg.innerText === '')) {
+    if (this.validity.valid && !(emailErrorMsg.innerText === '')) {
         emailErrorMsg.innerText = '';
     }
 }
