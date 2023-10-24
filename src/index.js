a7f7479e653e54214ef750752de8d71ac1c6d4e1
zawho <zthooper2@gmail.com>
2023-10-24 11:43:25 -0500

    Add country validation funcs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './style.css';
 
+function resetForm() {
+    const sampleForm = document.querySelector('.sample-form');
+    sampleForm.reset();
+}
+
+
 function addInvalidFocusStyle() {
     this.style.outline = '2px solid red';
 }
@@ -28,7 +34,7 @@ function validateEmailFocusOut() {
     }
 }
 
-function resetNeutralStyles() {
+function resetNeutralEmailStyles() {
     const emailErrorMsg = document.querySelector('#email-error-msg');
     if (this.validity.valueMissing) {
         this.style.border = '1px solid grey';
@@ -57,7 +63,52 @@ function setEmailEvents() {
     const emailInput = document.querySelector('.email-input');
     emailInput.addEventListener('focusout', validateEmailFocusOut);
     emailInput.addEventListener('input', validateEmailInput);
-    emailInput.addEventListener('input', resetNeutralStyles);
+    emailInput.addEventListener('input', resetNeutralEmailStyles);
 }
 
+function validateCountryFocusOut() {
+    const countryErrorMsg = document.querySelector('#country-error-msg');
+    if (this.validity.valueMissing) {
+        countryErrorMsg.innerText = 'please enter your country.';
+        this.style.border = '1px solid red';
+        this.removeEventListener('focus', addValidFocusStyle);
+        this.removeEventListener('focus', addNeutralFocusStyle);
+        this.addEventListener('focus', addInvalidFocusStyle);
+        this.addEventListener('focusout', removeFocusStyle);
+    }
+}
+
+function resetNeutralCountryStyles() {
+    const countryErrorMsg = document.querySelector('#country-error-msg');
+    if (this.validity.valueMissing && this.style.border === '1px solid green') {
+        this.style.border = '1px solid red';
+        this.style.outline = '2px solid red';
+        this.removeEventListener('focus', addValidFocusStyle);
+        this.addEventListener('focus', addInvalidFocusStyle);
+        countryErrorMsg.innerText = 'please enter your country.';
+    }
+}
+
+function validateCountryInput() {
+    const countryErrorMsg = document.querySelector('#country-error-msg');
+    if (this.validity.valid) {
+        this.style.border = '1px solid green';
+        this.style.outline = '2px solid green';
+        this.addEventListener('focus', addValidFocusStyle);
+        this.addEventListener('focusout', removeFocusStyle);
+    }
+    if (this.validity.valid && !(countryErrorMsg.innerText === '')) {
+        countryErrorMsg.innerText = '';
+    }
+}
+
+function setCountryEvents() {
+    const countryInput = document.querySelector('.country-input');
+    countryInput.addEventListener('focusout', validateCountryFocusOut);
+    countryInput.addEventListener('input', validateCountryInput);
+    countryInput.addEventListener('input', resetNeutralCountryStyles);
+}
+
+window.addEventListener('load', resetForm);
 setEmailEvents();
+setCountryEvents();
