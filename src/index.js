9ddbc75f6699d5deaa8633825e53a82607dae208
zawho <zthooper2@gmail.com>
2023-10-24 10:48:55 -0500

    Clean up email validation logic

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ function addValidFocusStyle() {
     this.style.outline = '2px solid green';
 }
 
+function addNeutralFocusStyle() {
+    this.style.outline = '2px solid blue';
+}
+
 function removeFocusStyle() {
     this.style.outline = 'none';
 }
@@ -19,8 +23,25 @@ function validateEmailFocusOut() {
         emailErrorMsg.innerText = 'please enter a valid email address.';
         emailInput.style.border = '1px solid red';
         emailInput.removeEventListener('focus', addValidFocusStyle);
+        emailInput.removeEventListener('focus', addNeutralFocusStyle);
         emailInput.addEventListener('focus', addInvalidFocusStyle);
         emailInput.addEventListener('focusout', removeFocusStyle);
+    } else if (emailInput.validity.valueMissing) {
+        emailInput.style.border = '1px solid grey';
+        emailInput.addEventListener('focus', addNeutralFocusStyle);
+    }
+}
+
+function resetNeutralStyles() {
+    const emailInput = document.querySelector('.email-input');
+    const emailErrorMsg = document.querySelector('#email-error-msg');
+    if (emailInput.validity.valueMissing) {
+        emailInput.style.border = '1px solid grey';
+        this.style.outline = '2px solid blue';
+        emailInput.addEventListener('focus', addNeutralFocusStyle);
+        emailInput.removeEventListener('focus', addValidFocusStyle);
+        emailInput.removeEventListener('focus', addInvalidFocusStyle);
+        emailErrorMsg.innerText = '';
     }
 }
 
@@ -38,10 +59,11 @@ function validateEmailInput() {
     }
 }
 
-function setEvents() {
+function setEmailEvents() {
     const emailInput = document.querySelector('.email-input');
     emailInput.addEventListener('focusout', validateEmailFocusOut);
     emailInput.addEventListener('input', validateEmailInput);
+    emailInput.addEventListener('input', resetNeutralStyles);
 }
 
-setEvents();
+setEmailEvents();
